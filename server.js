17ebd6afb20b93bb830cee53478febde10010124
UP940148 <up940148@myport.ac.uk>
2020-11-17 14:47:57 +0000

    Added file upload code

--- server.js
+++ server.js
@@ -4,10 +4,42 @@ const bodyParser = require('body-parser');
 const stayAwake = require('stay-awake');
 const config = require('./config');
 const googleAuth = require('simple-google-openid');
+const multer = require('multer');
 const db = require('./database.js');
+const fs = require('fs');
+const { promisify } = require('util');
+const renameAsync = promisify(fs.rename);
+
 
 // Create express app
 const app = express();
+
+const imgUploader = multer({
+  dest: config.imageStore,
+  limits: {
+    fields: 10,
+    fileSize: 1024 * 1024 * 20,
+    files: 1
+  },
+});
+const docUploader = multer({
+  dest: config.docStore,
+  limits: {
+    fields: 10,
+    fileSize: 1024 * 1024 * 20,
+    files: 1
+  },
+});
+
+app.post('/doc-upload/', docUploader.single('document'), async (req, res) => {
+  const fileExt = req.file.mimetype.split('/')[1];
+  await renameAsync(req.file.path, req.file.path + '.' + fileExt);
+
+});
+app.post('/img-upload/', imgUploader.single('avatar'), async (req, res) => {
+  const fileExt = req.file.mimetype.split('/')[1];
+  await renameAsync(req.file.path, req.file.path + '.' + fileExt);
+});
 
 const jsonParser = bodyParser.json();
 
@@ -21,6 +53,7 @@ app.listen(config.PORT, (err) => {
 
 app.use('/', express.static(config.www, { index: 'html/index.html', extenstions: ['HTML'] }));
 
+// POST
 app.post('/user/', jsonParser, async (req, res) => {
   const data = [req.body.googleId, req.body.name, req.body.displayName, req.body.profilePicture, req.body.email];
   let err = await db.addUser(data);
@@ -55,7 +88,69 @@ app.post('/reply/', jsonParser, async (req, res) => {
   res.json({
     success: true,
   });
+});
+
+app.post('/registration/', jsonParser, async (req, res) => {
+  const data = [req.body.groupId, req.body.userId, req.body.rankId];
+  let err = await db.addRegistration(data);
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({
+    success: true,
+  });
+});
+
+app.post('/grievance/', jsonParser, async (req, res) => {
+  const data = [req.body.prosecutorId, req.body.defendantId, req.body.documentId, req.body.replyId, req.body.content];
+  let err = await db.addGrievance(data);
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({
+    success: true,
+  });
+});
+
+app.post('/share/', jsonParser, async (req, res) => {
+  const data = [req.body.documentId, req.body.groupId];
+  let err = await db.addShare(data);
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({
+    success: true,
+  });
+});
+
+app.post('/group/', jsonParser, async (req, res) => {
+  const data = [req.body.groupName, req.body.groupPicture, req.body.groupDescription];
+  let err = await db.addGroup(data, req.body.userId);
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({
+    success: true,
+  });
+});
+
+app.post('/rank/', jsonParser, async (req, res) => {
+  const data = [req.body.groupId, req.body.rankName, req.body.level, req.body.colour, req.body.canPost, req.body.canReply, req.body.canRemove, req.body.canBan];
+  let err = db.addRank(data);
+  if (err) {
+    res.status(400).json({ error: err.message });
+    return;
+  }
+  res.json({
+    success: true,
+  });
 })
+
+// GET
 
 app.get('/users/u/:userId/', jsonParser, async (req, res) => {
   const id = [req.params.userId];
