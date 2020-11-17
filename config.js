68bae656e184647a39f486a9c0cf3731a6812e5d
UP940148 <up940148@myport.ac.uk>
2020-11-17 14:49:13 +0000

    Added export paths for uploaded files

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 const path = require('path');
-module.exports.www = path.join(__dirname, '/www/')
+module.exports.www = path.join(__dirname, '/www/');
+module.exports.imageStore = path.join(__dirname, '/www/uploads/img/');
+module.exports.docStore = path.join(__dirname, '/www/uploads/docs/')
 
 module.exports.PORT = 8080;
