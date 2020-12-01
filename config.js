e7534441b2b159c3e46b7aa327d33affbef227c9
UP940148 <62145437+UP940148@users.noreply.github.com>
2020-12-01 15:14:41 +0000

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-const path = require('path');
-module.exports.www = path.join(__dirname, '/www/');
-module.exports.uploads = path.join(__dirname, '/uploads/');
-module.exports.docStore = module.exports.www + '/docs/';
-module.exports.imageStore = module.exports.www + '/img/';
-module.exports.replyStore = module.exports.www + '/replies/';
-
-module.exports.PORT = 8080;
