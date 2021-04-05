d6d9870e268f98dd08d7bbff4d357f6b0d3cfff4
up940148 <up940148@myport.ac.uk>
2021-04-05 14:03:26 +0100

    Minor Tweaks

--- www/scripts/globalscripts.js
+++ www/scripts/globalscripts.js
@@ -7,7 +7,7 @@
 const colourPalette = [['#F0F2F5', '#FFFFFF', '#E4E6EB', '#212121', '#7b7b7b', '#6200EE'], ['#18191A', '#242526', '#3A3B3C', '#E4E6EB', '#B0B3B8', '#985EFF']];
 const root = document.documentElement;
 
-let currentColourMode = localStorage.getItem('LPRS_colourMode') || 'light';
+let currentColourMode = localStorage.getItem('LPRS_colourMode') || 'dark';
 const darkModeButton = document.getElementById('darkModeToggle');
 
 initPage();
