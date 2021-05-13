d7e49bd69ca963412b92aa3be3ab89f7aa8297e7
Daniel Ellis <up940148@myport.ac.uk>
2021-05-13 20:05:56 +0100

    :construction: Added database functions

--- server/database.js
+++ server/database.js
@@ -10,3 +10,58 @@ database.open(DBSOURCE)
   .catch(err => {
     console.log('Error connecting to database:', err);
   });
+
+// ADMIN FUNCTIONS
+
+// Retrieve all records from a given table
+exports.getAllInTable = async function (tableName) {
+  const sql = `SELECT * FROM ${tableName};`;
+  const response = await db.all(sql)
+    .then(rows => {
+      return { failed: false, context: rows };
+    })
+    .catch(err => {
+      return { failed: true, context: err };
+    });
+  return response;
+};
+
+// GENERAL QUERIES
+
+// Retrieve single record with a given primary key value from a given table
+exports.getRecordByPrimaryKey = async function (tableName, pKeyValue) {
+  const sql = `SELECT * FROM ${tableName} WHERE ${tableName}Id = ${pKeyValue};`;
+  const response = await db.get(sql)
+    .then(row => {
+      return { failed: false, context: row };
+    })
+    .catch(err => {
+      return { failed: true, context: err };
+    });
+  return response;
+};
+
+// Create Queries
+exports.createUser = async function (data) {
+  const sql = 'INSERT INTO user (userId, username, name, email, picture, savedQuestions) VALUES (?, ?, ?, ?, ?, ?);';
+  const response = await db.run(sql, data)
+    .then(() => {
+      return { failed: false, context: null };
+    })
+    .catch(err => {
+      return { failed: true, context: err.message };
+    });
+  return response;
+};
+
+exports.createCohort = async function (data) {
+  const sql = 'INSERT INTO cohort (name, description, isPrivate) VALUES (?, ?, ?);';
+  const response = await db.run(sql, data)
+    .then((record) => {
+      return { failed: false, context: { id: record.lastID } };
+    })
+    .catch(err => {
+      return { failed: true, context: err.message };
+    });
+  return response;
+};
